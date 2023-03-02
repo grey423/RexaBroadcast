edb9f65320ffdbc1038e8027f8e41273199790f1
Grey <95908917+grey423@users.noreply.github.com>
2023-03-01 22:37:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -247,6 +247,60 @@ LIST MENU BOT
         except:
             pass
 
+        
+    @app.on_message(filters.private)
+    async def incoming_private(_, message):
+        user_id = message.from_user.id
+        if await mongo.is_banned_user(user_id):
+            return
+        if user_id in SUDO_USERS:
+            if message.reply_to_message:
+                if (
+                    message.text == "/unblock"
+                    or message.text == "/block"
+                    or message.text == "/broadcast"
+                ):
+                    return
+                replied_id = message.reply_to_message_id
+                try:
+                    replied_user_id = save[replied_id]
+                except Exception as e:
+                    print(e)
+                    return await message.reply_text(
+                        "Failed to fetch user. You might've restarted bot or some error happened. Please check logs"
+                    )
+                try:
+                    return await app.copy_message(
+                        replied_user_id,
+                        message.chat.id,
+                        message.id,
+                    )
+                except Exception as e:
+                    print(e)
+                    return await message.reply_text(
+                        "Failed to send the message, User might have blocked the bot or something wrong happened. Please check logs"
+                    )
+        else:
+            if await mongo.is_group():
+                try:
+                    forwarded = await app.forward_messages(
+                        "-1001692951846",
+                        message.chat.id,
+                        message.id,
+                    )
+                    save[forwarded.id] = user_id
+                except:
+                    pass
+            else:
+                for user in SUDO_USERS:
+                    try:
+                        forwarded = await app.forward_messages(
+                            user, message.chat.id, message.id
+                        )
+                        save[forwarded.id] = user_id
+                    except:
+                        pass
+                    
     print("[Bot Posting] - Bot Posting Started")
     await idle()
 
